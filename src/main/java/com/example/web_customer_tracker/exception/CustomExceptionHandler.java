4a07cdd2f50479fa127bd5e5236ffb520a7e5711
alihmzyv <alihmzyv@gmail.com>
2022-11-11 21:51:40 +0400

    handle constraint violation exceptions, return the message of the most specific cause which is much more informative

--- src/main/java/com/example/web_customer_tracker/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/web_customer_tracker/exception/CustomExceptionHandler.java
@@ -1,33 +1,28 @@
 package com.example.web_customer_tracker.exception;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.validation.ValidationException;
-import java.awt.font.NumericShaper;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<CustomerErrorResponse> handleNotFoundException(NotFoundException exc) {
+    public ResponseEntity<CustomerErrorResponse> handleNotFound(NotFoundException exc) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(), exc.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<CustomerErrorResponse> handleAll(Throwable exc) {
+    public ResponseEntity<CustomerErrorResponse> handleConstrainViolation(DataIntegrityViolationException exc) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(), exc.getMessage()));
+                .body(new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(), exc.getMostSpecificCause().getMessage()));
     }
 
     public static List<CustomerErrorResponse> getErrorResponses(Errors errors) {
