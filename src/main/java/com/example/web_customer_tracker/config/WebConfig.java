85116e814957643d3c0c882acbb237ef218085c3
alihmzyv <alihmzyv@gmail.com>
2022-11-12 00:18:12 +0400

    seperated data source config from web config

--- src/main/java/com/example/web_customer_tracker/config/WebConfig.java
+++ src/main/java/com/example/web_customer_tracker/config/WebConfig.java
@@ -14,30 +14,9 @@ import javax.sql.DataSource;
 import java.util.logging.Logger;
 
 @Configuration
-@PropertySource("classpath:data.source.properties")
 public class WebConfig implements WebMvcConfigurer {
-    private final Logger logger = Logger.getLogger(this.getClass().getName());
-    @Autowired
-    private Environment env;
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-    }
-
-    @Bean
-    public DataSource securityDataSource() {
-        SimpleDriverDataSource dataSource =  DataSourceBuilder.create().type(SimpleDriverDataSource.class)
-                .driverClassName(env.getProperty("jdbc.driver"))
-                .url(env.getProperty("jdbc.url"))
-                .username(env.getProperty("jdbc.username"))
-                .password(env.getProperty("jdbc.password"))
-                .build();
-        logger.info(String.format("Data Source built: Driver=%s, url=%s, username=%s, password=%s",
-                dataSource.getDriver().getClass().getName(),
-                dataSource.getUrl(),
-                dataSource.getUsername(),
-                dataSource.getPassword()));
-        return dataSource;
     }
 }
