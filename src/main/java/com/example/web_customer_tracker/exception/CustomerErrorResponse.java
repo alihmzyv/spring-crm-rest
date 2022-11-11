8be3c7bbf15f81bcf3b567c264edf27a11a0a58f
alihmzyv <alihmzyv@gmail.com>
2022-11-12 00:15:43 +0400

    removed unused setters, non-arg constructor

--- src/main/java/com/example/web_customer_tracker/exception/CustomerErrorResponse.java
+++ src/main/java/com/example/web_customer_tracker/exception/CustomerErrorResponse.java
@@ -3,35 +3,24 @@ package com.example.web_customer_tracker.exception;
 import java.time.Instant;
 
 public class CustomerErrorResponse {
-    private int statusCode;
-    private String message;
-    private long timeStamp = Instant.now().toEpochMilli();
+    private final int statusCode;
+    private final String message;
+    private final long timeStamp = Instant.now().toEpochMilli();
 
     public CustomerErrorResponse(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
 
-    public CustomerErrorResponse() {
-    }
-
-    //getter and setters
     public int getStatusCode() {
         return statusCode;
     }
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
+
     public String getMessage() {
         return message;
     }
-    public void setMessage(String message) {
-        this.message = message;
-    }
+
     public long getTimeStamp() {
         return timeStamp;
-    }
-    public void setTimeStamp(long timeStamp) {
-        this.timeStamp = timeStamp;
     }
 }
