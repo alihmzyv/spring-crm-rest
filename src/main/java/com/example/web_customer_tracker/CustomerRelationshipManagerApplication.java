7a509628c41db265fd3e2ceba595e6591de70eb1
alihmzyv <alihmzyv@gmail.com>
2022-11-12 00:16:11 +0400

    removed using xml as resource

--- src/main/java/com/example/web_customer_tracker/CustomerRelationshipManagerApplication.java
+++ src/main/java/com/example/web_customer_tracker/CustomerRelationshipManagerApplication.java
@@ -3,10 +3,11 @@ package com.example.web_customer_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@ImportResource("classpath:spring-mvc-demo.xml")
 public class CustomerRelationshipManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerRelationshipManagerApplication.class, args);
