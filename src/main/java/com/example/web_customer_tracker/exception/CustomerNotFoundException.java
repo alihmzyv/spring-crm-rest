93bbed0b90103cc9424349747939e75ea52c9e1b
alihmzyv <alihmzyv@gmail.com>
2022-11-17 23:08:27 +0400

    deleted unused constructors

--- src/main/java/com/example/web_customer_tracker/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/web_customer_tracker/exception/CustomerNotFoundException.java
@@ -1,22 +1,8 @@
 package com.example.web_customer_tracker.exception;
 
 public class CustomerNotFoundException extends NotFoundException {
-    public CustomerNotFoundException() {
-    }
 
     public CustomerNotFoundException(String message) {
         super(message);
-    }
-
-    public CustomerNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CustomerNotFoundException(Throwable cause) {
-        super(cause);
-    }
-
-    public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
