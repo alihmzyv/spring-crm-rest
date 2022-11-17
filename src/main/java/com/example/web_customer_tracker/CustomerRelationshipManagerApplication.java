709b525848164efe0c0919495623bd8370f3e2c4
alihmzyv <alihmzyv@gmail.com>
2022-11-17 23:09:39 +0400

    validation exception handling delegated to exception handler class

--- src/main/java/com/example/web_customer_tracker/CustomerRelationshipManagerApplication.java
+++ src/main/java/com/example/web_customer_tracker/CustomerRelationshipManagerApplication.java
@@ -2,12 +2,8 @@ package com.example.web_customer_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Component;
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@SpringBootApplication
 public class CustomerRelationshipManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerRelationshipManagerApplication.class, args);
--- src/main/java/com/example/web_customer_tracker/rest/controller/CustomerRestController.java
+++ src/main/java/com/example/web_customer_tracker/rest/controller/CustomerRestController.java
@@ -2,30 +2,36 @@ package com.example.web_customer_tracker.rest.controller;
 
 
 import com.example.web_customer_tracker.entity.Customer;
-import com.example.web_customer_tracker.exception.CustomExceptionHandler;
 import com.example.web_customer_tracker.exception.CustomerNotFoundException;
 import com.example.web_customer_tracker.service.CustomerService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping
 public class CustomerRestController {
+    private final CustomerService customerService;
+
     @Autowired
-    private CustomerService customerService;
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping("/customers")
-    public List<Customer> getCustomers() {
-        return customerService.getCustomers();
+    public Page<Customer> getCustomers(@RequestParam(defaultValue = "0") int page,
+                                       @RequestParam(defaultValue = "3") int size,
+                                       @RequestParam(defaultValue = "") List<String> sort) {
+        return customerService.findAll(page, size, sort);
     }
 
     @GetMapping("/customers/{customerId}")
+    @SneakyThrows
     public Customer getCustomer(@PathVariable long customerId) {
         return customerService.getCustomerById(customerId)
                 .orElseThrow(() ->
@@ -33,32 +39,20 @@ public class CustomerRestController {
     }
 
     @PostMapping("/customers")
-    public ResponseEntity addCustomer(@RequestBody @Valid Customer customer, Errors errors) {
-        if (errors.hasErrors()) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(CustomExceptionHandler.getErrorResponses(errors));
-        }
-        customer.setId(null);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody @Valid Customer customer) {
         customerService.saveOrUpdateCustomer(customer);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(customer);
+        return customer;
     }
 
-    @PutMapping("/customers")
-    public ResponseEntity updateCustomer(@RequestBody @Valid Customer customer, Errors errors) {
-        if (errors.hasErrors()) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(CustomExceptionHandler.getErrorResponses(errors));
-        }
-        if (customer.getId() == null || customerService.getCustomerById(customer.getId()).isEmpty()) {
+    @PutMapping("/customers/{customerId}")
+    public Customer updateCustomer(@PathVariable Long customerId, @RequestBody @Valid Customer customer) {
+        if (customerService.getCustomerById(customerId).isEmpty()) {
             throw new CustomerNotFoundException(String.format("Customer could not be found: id = %d", customer.getId()));
         }
+        customer.setId(customerId);
         customerService.saveOrUpdateCustomer(customer);
-        return ResponseEntity
-                .ok(customer);
+        return customer;
     }
 
     @DeleteMapping("/customers/{customerId}")
